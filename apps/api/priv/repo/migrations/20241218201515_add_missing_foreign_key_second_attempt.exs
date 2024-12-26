e4ea31686dce23d30fe79614ec17e50e0a2e92ed
Wojtazz <marcin.witas72@gmail.com>
2024-12-26 13:17:16 +0100

    fix(api): remove invalid migration

--- apps/api/priv/repo/migrations/20241218201515_add_missing_foreign_key_second_attempt.exs
+++
@@ -1,9 +0,0 @@
-defmodule Api.Repo.Migrations.AddMissingForeignKeySecondAttempt do
-  use Ecto.Migration
-
-  def change do
-    alter table(:posts) do
-      add :user_id, :integer
-    end
-  end
-end
