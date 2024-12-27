affe82aabacbd33131a3a7308d8e15b3c5659044
Wojtazz <marcin.witas72@gmail.com>
2024-12-27 18:31:35 +0100

    refactor(web): change button color

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -11,7 +11,7 @@ export const Button = ({ type, children }: ButtonProps) => {
 	return (
 		<button
 			type={type}
-			className="w-full bg-blue-600 text-white py-2 rounded-lg font-semibold hover:bg-blue-700 transition"
+			className="w-full bg-[#0866ff] text-white py-2 px-4 rounded-lg font-semibold hover:bg-blue-700 transition"
 		>
 			{children}
 		</button>
