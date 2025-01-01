0ee7d63ff6d59b9d712e640070c659aa7a17fdc6
Wojtazz <marcin.witas72@gmail.com>
2025-01-01 11:15:45 +0100

    refactor(api): remove html responses and comment unused actions

--- apps/api/lib/api_web/controllers/user_confirmation_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule ApiWeb.UserConfirmationHTML do
-  use ApiWeb, :html
-
-  embed_templates "user_confirmation_html/*"
-end
--- apps/api/lib/api_web/controllers/user_reset_password_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule ApiWeb.UserResetPasswordHTML do
-  use ApiWeb, :html
-
-  embed_templates "user_reset_password_html/*"
-end
