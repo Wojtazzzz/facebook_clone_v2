922cdde8542194121b52826b42485cffff70e0e6
Marcin Witas <cubestorm72@gmail.com>
2025-01-06 21:01:43 +0100

    feat(api): add user account to seeders

--- apps/api/priv/repo/seeds.exs
+++ apps/api/priv/repo/seeds.exs
@@ -9,3 +9,17 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Api.Repo
+alias Api.Accounts.{User}
+
+Repo.delete_all(User)
+
+Repo.insert(%User{
+  email: "marcin.witas72@gmail.com",
+  hashed_password: Bcrypt.hash_pwd_salt("admin"),
+  confirmed_at: nil,
+  image_url: "https://gravatar.com/avatar/b0a8c4bbc0ef26b4f5be6d8f2af30634?s=400&d=robohash&r=x",
+  first_name: "Marcin",
+  last_name: "Witas"
+})
