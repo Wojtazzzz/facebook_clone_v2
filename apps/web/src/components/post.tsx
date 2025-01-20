23695413967f8b2590ba99d34db07ee7dedb4c4e
Marcin Witas <cubestorm72@gmail.com>
2025-01-20 20:50:03 +0100

    refactor(web): improve post reactions ui

--- apps/web/src/components/post.tsx
+++ apps/web/src/components/post.tsx
@@ -60,11 +60,11 @@ export const Post = ({ post }: PostProps) => {
 			</div>
 
 			{/* Reactions */}
-			<div className="px-4 py-2 flex items-center gap-1 border-b">
-				<span className="inline-flex items-center justify-center w-4 h-4">
-					❤️
+			<div className="px-4 py-2 flex items-center gap-x-2 border-b">
+				<span className="flex items-center w-4 h-4 text-[#0866ff]">
+					<LikeIcon size={20} />
 				</span>
-				<span className="text-sm text-gray-500">12</span>
+				<span className="text-sm text-gray-400">12</span>
 			</div>
 
 			{/* Actions */}
